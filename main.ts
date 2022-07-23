edc6bb1b2d0aecf50bbddfe0a8dc8dc0bdc751c0
Informática CNBA <105891917+INFOCNBA@users.noreply.github.com>
2022-07-23 11:21:36 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+/**
+ * Enunciado1+:
+ * 
+ * Este es el programa mas básico qué podemos tener  :)
+ * 
+ * - aca vemos un solo evento (que es atendido por una sola primitiva)
+ * 
+ * Desafío:
+ * 
+ * - modificar el evento del botón A para dar la primera pista
+ * 
+ * - programar dos eventos más para dar las siguientes pistas del deporte escondido
+ * 
+ * Desafío++:
+ * 
+ * - que el evento del botón A sea un icono
+ * 
+ * - que el evento del botón B además de la pista, toque un sonido
+ */
 // Este es el primer bloque que pacticaremos: es un Evento
 input.onButtonPressed(Button.A, function () {
     basic.showString("pistaA")
