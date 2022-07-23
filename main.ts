ab24aaec983974505f8d03cc584d1247db9090a2
Informática CNBA <105891917+INFOCNBA@users.noreply.github.com>
2022-07-23 10:49:18 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+// Este es el primer bloque que pacticaremos: es un Evento
+input.onButtonPressed(Button.A, function () {
+    basic.showString("pistaA")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("pistaAB")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("pistaB")
+})
